9568329cf8ce44784b60b98b5a6fddebfb68d046
ardae <45439333+ardaertanyildiz@users.noreply.github.com>
2019-09-07 23:42:05 +1000

    Add files via upload

---
+++ Flock/FlockSystem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+
+
+namespace Haecceity.Flock
+{
+    public class FlockSystem
+    {
+        public List<FlockAgent> Agents;
+
+        public double Timestep;
+        public double NeighbourhoodRadius;
+        public double AlignmentStrength;
+        public double CohesionStrength;
+        public double SeparationStrength;
+        public double SeparationDistance;
+        public List<Circle> Repellers;
+        public bool UseParallel;
+
+
+        public FlockSystem(int agentCount, bool flock3D)
+        {
+            Agents = new List<FlockAgent>();
+
+            //flock3d
+            if (flock3D)
+                for (int i = 0; i < agentCount; i++)
+                {
+                    FlockAgent agent = new FlockAgent(
+                        Util.GetRandomPoint(0.0, 30.0, 0.0, 30.0, 0.0, 30.0),
+                        Util.GetRandomUnitVector() * 4.0);
+
+                    agent.FlockSystem = this;
+
+                    Agents.Add(agent);
+                }
+            //flock2d
+            else
+                for (int i = 0; i < agentCount; i++)
+                {
+                    FlockAgent agent = new FlockAgent(
+                        Util.GetRandomPoint(0.0, 30.0, 0.0, 30.0, 0.0, 0.0),
+                        Util.GetRandomUnitVectorXY() * 4.0);
+
+                    agent.FlockSystem = this;
+
+                    Agents.Add(agent);
+                }
+        }
+
+
+        private List<FlockAgent> FindNeighbours(FlockAgent agent)
+        {
+            List<FlockAgent> neighbours = new List<FlockAgent>();
+
+            foreach (FlockAgent neighbour in Agents)
+                if (neighbour != agent && neighbour.Position.DistanceTo(agent.Position) < NeighbourhoodRadius)
+                    neighbours.Add(neighbour);
+
+            return neighbours;
+        }
+
+
+        public void Update()
+        {
+            foreach (FlockAgent agent in Agents)
+            {
+                List<FlockAgent> neighbours = FindNeighbours(agent);
+                agent.ComputeDesiredVelocity(neighbours);
+            }
+
+            // Once the desired velocity for each agent has been computed, we update each position and velocity
+            foreach (FlockAgent agent in Agents)
+                agent.UpdateVelocityAndPosition();
+        }
+    }
+}
---
+++ HaecceityInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Haecceity
+{
+    public class HaecceityInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Haecceity";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("f79ba8c7-eb7a-4100-aab7-b4c07b263ac8");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "ardae";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
